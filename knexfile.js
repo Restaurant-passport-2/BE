91ae578f8cbf400f09ff0c0ba3cdb7c96001f527
Michael Nunes <miketnunes@gmail.com>
2020-01-04 22:13:52 -0800

    Add knex config & database folders (seeds/migrations)

---
+++ knexfile.js
@@ -0,0 +1,34 @@
+module.exports = {
+  development: {
+    client: "pg",
+    connection: process.env.DATABASE_URL_DEV,
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+
+  testing: {
+    client: "pg",
+    connection: process.env.DATABASE_URL_TEST,
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+  },
+};
