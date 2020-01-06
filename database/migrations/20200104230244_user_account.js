8aa19d8b5f5bb71aa266c8ca833126b66f7ddd1d
Michael Nunes <miketnunes@gmail.com>
2020-01-06 13:00:37 -0800

    Add migrations for other tables

--- database/migrations/20200104230244_user_account.js
+++ database/migrations/20200104230244_user_account.js
@@ -2,20 +2,25 @@ exports.up = function(knex) {
   return knex.schema.createTable("user_account", (tbl) => {
     tbl.increments();
 
+    //Required Unique email
     tbl
       .string("email")
       .unique()
       .notNullable();
 
+    //Required Unique username
     tbl
       .string("username")
       .unique()
       .notNullable();
 
+    //Required password
     tbl.string("password").notNullable();
 
+    //Required city
     tbl.string("city").notNullable();
 
+    //Required zipcode
     tbl.string("zipcode").notNullable();
   });
 };
