fce6e91e8b4b0add3ab75df798af676f434fdf6c
Michael Nunes <miketnunes@gmail.com>
2020-01-07 14:01:26 -0800

    Add seeds for existing tables

--- database/seeds/02-users.js
+++ database/seeds/02-users.js
@@ -1,14 +1,17 @@
+require("dotenv").config();
 const bcrypt = require("bcrypt");
+
+const saltRounds = Number(process.env.HASH_SALT_ROUNDS);
 
 exports.seed = function(knex) {
   return knex("user_account").insert([
     {
-      name: "demo",
-      email: "demo@email.com",
-      username: "demo",
-      password: bcrypt.hashSync("demo", 8),
-      city: "Demo Town",
-      zipcode: "12345",
+      name: "test",
+      email: "test@email.com",
+      username: "test",
+      password: bcrypt.hashSync("test", saltRounds),
+      city: "Santa Clarita",
+      zipcode: "91350",
     },
   ]);
 };
--- database/seeds/04-restaurants.js
+++ database/seeds/04-restaurants.js
@@ -1,1 +1,10 @@
-exports.seed = function(knex) {};
+exports.seed = function(knex) {
+  return knex("restaurant").insert({
+    name: "Chi Chi's Pizza",
+    street_address: "23043 Soledad Canyon Rd",
+    city: "Santa Clarita",
+    state: "CA",
+    zipcode: "91350",
+    phone_number: "(661) 259-4040",
+  });
+};
