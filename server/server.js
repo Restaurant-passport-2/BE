b349cbb849e4d7a2a7fbe3ea603595c5545899ba
Michael Nunes <miketnunes@gmail.com>
2020-01-07 08:39:01 -0800

    Enable cors for all origins for testing...

--- server/server.js
+++ server/server.js
@@ -10,19 +10,19 @@ const server = express();
 server.use(express.json());
 
 server.use(helmet());
-const whitelist = process.env.NODE_ENV === "development" ? ["http://localhost:3000"] : [];
-const corsOptions = {
-  origin: function(origin, callback) {
-    //Check for whitelisted origin or self origin.
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// const whitelist = process.env.NODE_ENV === "development" ? ["http://localhost:3000"] : [];
+// const corsOptions = {
+//   origin: function(origin, callback) {
+//     //Check for whitelisted origin or self origin.
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
-server.use(cors(corsOptions));
+server.use(cors());
 
 server.use("/api", apiRouter);
 
