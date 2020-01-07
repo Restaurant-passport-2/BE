14265a6d643f1f111d5e0f388bb43e0e56cad9b8
Michael Nunes <miketnunes@gmail.com>
2020-01-06 23:36:36 -0800

    Add auto-generated api-docs for API

---
+++ api-docs/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "title": "Restaurant Passport API Documentation",
+  "name": "Restaurant Passport API Documentation",
+  "description": "For the Restaurant Passport 2 node API server.",
+  "version": "1.0.0",
+  "url": "https://restaurant-passport-2.herokuapp.com/api",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-01-07T07:33:49.203Z",
+    "url": "http://apidocjs.com",
+    "version": "0.19.1"
+  }
+});
