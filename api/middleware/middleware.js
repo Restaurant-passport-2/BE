66a478d6f5de1f83d4da234d6aa4f5eafc389612
Michael Nunes <miketnunes@gmail.com>
2020-01-07 14:03:31 -0800

    Add middleware to validate new passport entries

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,6 @@
 const validateLogin = require("./validateLogin");
 const validateSignup = require("./validateSignup");
+const validateEntry = require("./validateEntry");
 const searchBuilder = require("./searchBuilder");
 const internalError = require("./internalError");
 const authenticator = require("./authenticator");
@@ -8,6 +9,7 @@ const signToken = require("./signToken");
 module.exports = {
   validateLogin,
   validateSignup,
+  validateEntry,
   searchBuilder,
   internalError,
   authenticator,
