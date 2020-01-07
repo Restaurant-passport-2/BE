a6b829ca5014291f353753b942e7833f20ef0beb
Michael Nunes <miketnunes@gmail.com>
2020-01-07 14:53:17 -0800

    Update api-docs

--- api-docs/api_project.js
+++ api-docs/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-01-07T17:55:32.818Z",
+    "time": "2020-01-07T22:52:56.048Z",
     "url": "http://apidocjs.com",
     "version": "0.19.1"
   }
