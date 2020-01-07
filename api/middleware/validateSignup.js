6f1d860cd564a222156e9d69b695795cdd1db4d5
Michael Nunes <miketnunes@gmail.com>
2020-01-06 21:30:35 -0800

    Add missing req.user assignment for signup validator

--- api/middleware/validateSignup.js
+++ api/middleware/validateSignup.js
@@ -2,6 +2,7 @@ module.exports = function validateSignup(req, res, next) {
   const { name, email, username, password, city, zipcode } = req.body;
 
   if (name && email && username && password && city && zipcode) {
+    req.user = { name, email, username, password, city, zipcode };
     next();
   } else {
     res.status(400).json({ message: "Please provide name, email, username, password, city, and zipcode" });
