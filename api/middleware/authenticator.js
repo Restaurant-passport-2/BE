cd04a2302813936a1e513401adc0bfdadf8b2798
Michael Nunes <miketnunes@gmail.com>
2020-01-06 20:52:14 -0800

    Add jwt authenticator middleware

---
+++ api/middleware/authenticator.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function(req, res, next) {
+  const { authorization } = req.headers;
+
+  if (authorization) {
+    const secret = process.env.JWT_SECRET;
+    jwt.verify(authorization, secret, function(error, decodedToken) {
+      if (error) {
+        res.status(401).json({ message: "Invalid token" });
+      } else {
+        req.token = decodedToken;
+        next();
+      }
+    });
+  } else {
+    res.status(401).json({ message: "User must be logged in to access this resource" });
+  }
+};
