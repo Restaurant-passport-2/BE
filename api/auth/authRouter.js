d6022e193dbebb304be322b82d9f7b1eea9c8364
Michael Nunes <miketnunes@gmail.com>
2020-01-06 20:54:25 -0800

    Convert to async password hash method

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
-const { validateSignup, databaseError } = require("../middleware/middleware");
 const userDB = require("../dbHelpers/Users");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const { validateSignup, databaseError } = require("../middleware/middleware");
 
 router.post("/login", function(req, res) {
   const { username, password } = req.body;
@@ -11,15 +12,20 @@ router.post("/login", function(req, res) {
 
 router.post("/signup", validateSignup, function(req, res) {
   const user = ({ name, email, username, password, city, zipcode } = req.body);
-  user.password = bcrypt.hashSync(user.password, Number(process.env.HASH_SALT_ROUNDS));
-
-  userDB
-    .insert(user)
-    .then((user_id) => {
-      res.status(201).json({ userId: user_id[0] });
+  bcrypt
+    .hash(user.password, Number(process.env.HASH_SALT_ROUNDS))
+    .then((hash) => {
+      userDB
+        .insert(user)
+        .then((user_id) => {
+          res.status(201).json({ userId: user_id[0] });
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
     })
     .catch((err) => {
-      res.status(500).json(err);
+      res.status(500).json({ message: "Error generating hash", error: error });
     });
 });
 
