d174bcdce10db42dfbe8eb8ca8f92053de0a03c2
Michael Nunes <miketnunes@gmail.com>
2020-01-06 21:26:42 -0800

    Add new middleware to master import file

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,7 +1,13 @@
+const validateLogin = require("./validateLogin");
 const validateSignup = require("./validateSignup");
 const databaseError = require("./databaseError");
+const authenticator = require("./authenticator");
+const signToken = require("./signToken");
 
 module.exports = {
+  validateLogin,
   validateSignup,
   databaseError,
+  authenticator,
+  signToken,
 };
