2ca77f184f7a85f5db7cb9df38ba0c6b9618979c
Michael Nunes <miketnunes@gmail.com>
2020-01-08 15:12:08 -0800

    Add DB CRUD for passport

--- api/dbHelpers/Passport.js
+++ api/dbHelpers/Passport.js
@@ -3,7 +3,9 @@ const restaurant = require("./Restaurant");
 
 module.exports = {
   find,
-  // insert,
+  insertEntry,
+  updateEntry,
+  removeEntry,
   user,
   restaurant,
 };
@@ -17,4 +19,19 @@ async function find(user_id) {
   } else {
     return { entries: [] };
   }
+}
+
+async function insertEntry(user_id, newRestaurant) {
+  await restaurant.insert(newRestaurant);
+  return find(user_id);
+}
+
+async function updateEntry(user_id, restaurant_id, changes) {
+  await restaurant.update(restaurant_id, changes);
+  return find(user_id);
+}
+
+async function removeEntry(user_id, restaurant_id) {
+  await restaurant.remove(restaurant_id);
+  return find(user_id);
 }
