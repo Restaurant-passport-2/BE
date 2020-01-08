77477eedd8669ad2626e7fce30af9b1dfd6025e4
Michael Nunes <miketnunes@gmail.com>
2020-01-08 10:09:47 -0800

    Changed error responses to use an error key instead of message

--- api/middleware/validateEntry.js
+++ api/middleware/validateEntry.js
@@ -15,11 +15,9 @@ module.exports = function validateEntry(req, res, next) {
   if (name && street_address && city && state && zipcode && personal_rating && notes) {
     next();
   } else {
-    res
-      .status(400)
-      .json({
-        message:
-          "Please provide at minimum a name, street address, city, state, zipcode, personal rating, and notes to create a new entry",
-      });
+    res.status(400).json({
+      error:
+        "Please provide at minimum a name, street address, city, state, zipcode, personal rating, and notes to create a new entry",
+    });
   }
 };
