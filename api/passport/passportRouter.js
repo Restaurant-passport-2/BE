af4039d99b380dce20a97c8916a8b1be8692894a
Michael Nunes <miketnunes@gmail.com>
2020-01-09 10:07:41 -0800

    Update api docs for passport router

--- api/passport/passportRouter.js
+++ api/passport/passportRouter.js
@@ -7,7 +7,7 @@ const { authenticator, validateEntry, internalError } = require("../middleware/m
  * @apiName GetPassport
  * @apiGroup Passport
  *
- * @apiSuccessExample Success-Response:
+ * @apiSuccessExample Success-Response
  *    HTTP/1.1 200 OK
   {
     "entries": [
@@ -162,7 +162,7 @@ router.post("/entry", authenticator, validateEntry, function(req, res) {
  *
  * @apiParam {Integer} id restaurant id to delete.
  *
- * @apiSuccessExample Success-Response:
+ * @apiSuccessExample Success-Response
  *    HTTP/1.1 200 OK
   {
     "entries": [
@@ -195,6 +195,9 @@ router.post("/entry", authenticator, validateEntry, function(req, res) {
     ]
   }
  *
+ * @apiSuccessExample Alternate-Response
+ *    HTTP/1.1 204 No Content
+ * 
  * @apiError (401 Unauthorized) {json} Unauthorized Missing or invalid token in authorization header.
  *
  * @apiErrorExample {json} 401 Error-Response
@@ -253,7 +256,7 @@ router.put("/entry/:restaurant_id", authenticator, function(req, res) {
  *
  * @apiParam {Integer} id restaurant id to delete.
  *
- * @apiSuccessExample Success-Response:
+ * @apiSuccessExample Success-Response
  *    HTTP/1.1 200 OK
   {
     "entries": [
@@ -286,6 +289,9 @@ router.put("/entry/:restaurant_id", authenticator, function(req, res) {
     ]
   }
  *
+ * @apiSuccessExample Alternate-Response
+ *    HTTP/1.1 204 No Content
+ * 
  * @apiError (401 Unauthorized) {json} Unauthorized Missing or invalid token in authorization header.
  *
  * @apiErrorExample {json} 401 Error-Response
